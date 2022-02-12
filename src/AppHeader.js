f12301b28d3ecfbb4f9d4bffebc87c142a7ee990
channels2696 <davidcanalesr4@gmail.com>
2022-02-12 15:35:28 -0600

    reyca version 0.5.1

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,9 +1,9 @@
 
-import logo from './logoReyca.png'
-import React from 'react'
-import {Outlet, Link } from "react-router-dom"
-import WebFont from 'webfontloader'
-import './App.css'
+import logo from './logoReyca.png';
+import React from 'react';
+import {Outlet, Link } from "react-router-dom";
+import WebFont from 'webfontloader';
+import './App.css';
 
 
 WebFont.load({
@@ -14,13 +14,13 @@ WebFont.load({
 
 const AppHeader = () => {
   return (
-    <div style={{height:'100vh',backgroundColor:'dodgerblue'}}>
+    <div style={{backgroundColor:'dodgerblue'}}>
       <div style={{height:'12vh', display:'flex'}}>
         <img src={logo} alt="logo de Reyca" style={{height:'10vh',marginLeft:'5%'}}></img>
         <nav style={{position:'absolute',top:'1vh',right:'2vh'}}>
-          <Link className="button" to="/productos">Productos</Link>
-          <Link className="button" to="/empresarial">Empresarial</Link>
-          <Link className="button" to="/contactenos">Contactenos</Link>
+          <Link className="button" to="/nav/productos">Productos</Link>
+          <Link className="button" to="/nav/empresarial">Empresarial</Link>
+          <Link className="button" to="/nav/contactenos">Contactenos</Link>
         </nav>
       </div>
       <Outlet/>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import './index.css';
 import AppHeader from './AppHeader';
-import Productos from './routes/productos';
-import Empresarial from './routes/empresarial';
-import Contactenos from './routes/contactenos';
+import Productos from './productos';
+import Empresarial from './empresarial';
+import Contactenos from './contactenos';
 import Home from './home';
 import reportWebVitals from './reportWebVitals';
 import Amplify from 'aws-amplify';
@@ -16,8 +16,12 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AppHeader/>}>
-          <Route path="home" element={<Home/>}/>
+        <Route path="/" element={<div
+          style={{height:'100vh', backgroundColor:'dodgerblue'}}>
+                                <AppHeader/>
+                                <Home/>
+                                </div>}/>
+        <Route path="/nav" element={<AppHeader/>}>
           <Route path="productos" element={<Productos />} />
           <Route path="empresarial" element={<Empresarial />} />
           <Route path="contactenos" element={<Contactenos />}/>
